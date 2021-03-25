b5b44a28d8b3f515e580b1543be64c561622c527
mlouward <mlouward@gmail.com>
2021-03-25 17:08:20 +0100

    added list of stations + station search to the app

--- server_flask/static/js/index.js
+++ server_flask/static/js/index.js
@@ -1,12 +1,76 @@
 const listStations = document.getElementById("list-stations")
+const searchbar = document.getElementById("searchbar")
+
+searchbar.addEventListener('input', _ => {
+  // Search with regex (case-insensititve and anywhere in the name/adddress of bike station)
+  const searchTerms = escapeRegExp(searchbar.value);
+  if (searchTerms === "") return window.stations;
+  const exp = new RegExp(searchTerms, 'ig');
+  // filter regex matches on address or name of station
+  const searchResults = window.stations.filter((x) => {
+    if (x.name.search(exp) > -1 || x.address.search(exp) > -1)
+      return x;
+  })
+
+  // remove all cards
+  while (listStations.firstChild) {
+    listStations.firstChild.remove()
+  }
+  // sort results
+  searchResults.sort((a, b) => (a.name > b.name) ? 1 : -1)
+  // add the matching cards
+  addCards(searchResults);
+})
+
 
 function displayBikestands(stations) {
+  // Don't display stations with no name
+  window.stations = stations.filter((x) => { return x.name !== '' })
+  // Sort by alphabetical order
+  window.stations.sort((a, b) => (a.name > b.name) ? 1 : -1)
+
   // iterate over all of our stations and create a card for each of them
   // with required info
+  addCards(window.stations)
+}
+
+function addCards(stations) {
   for (const s of stations) {
     // Create card element and add requiered info
-    document.createElement('li')
+    const card = document.createElement('div')
+    card.classList = 'card';
+
+    const stationName = document.createElement('h2')
+    stationName.innerHTML = `${s.name}`
+    card.appendChild(stationName)
+
+    const stationAddress = document.createElement('p')
+    stationAddress.innerHTML = `${s.address}`
+    card.appendChild(stationAddress)
+
+    const bikeWrapper = document.createElement('div')
+    bikeWrapper.classList = 'bike-wrapper'
+    card.appendChild(bikeWrapper)
+
+    const availableBikes = document.createElement('p')
+    availableBikes.innerHTML = `Vélos disponibles: ${s.available_bikes}`
+    bikeWrapper.appendChild(availableBikes)
+
+    const availableBikeStands = document.createElement('p')
+    availableBikeStands.innerHTML = `Places disponibles: ${s.available_bike_stands}`
+    availableBikeStands.style = 'margin-left: auto'
+    bikeWrapper.appendChild(availableBikeStands)
+
+    const lastUpdate = document.createElement('p')
+    lastUpdate.style = 'text-align: right; color: #666'
+    lastUpdate.innerHTML = `Last update: ${s.last_update}`
+    card.appendChild(lastUpdate)
+
     // Add element to our list
-    listStations.appendChild(station)
+    listStations.appendChild(card)
   }
+}
+
+function escapeRegExp(s) {
+  return s.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&')
 }
